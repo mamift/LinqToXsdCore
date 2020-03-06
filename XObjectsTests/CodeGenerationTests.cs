7dc56fb8722406464c965e1070ffc8fcbcf32434
Muhammad Miftah <mamift@gmail.com>
2020-03-06 13:14:52 +1000

    Wrote unit tests for previous 2 pull requests.
    Found some issues...

--- XObjectsTests/CodeGenerationTests.cs
+++ XObjectsTests/CodeGenerationTests.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 using System.Xml;
+using System.Xml.Resolvers;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -13,6 +16,8 @@ using Xml.Schema.Linq.CodeGen;
 
 namespace Xml.Schema.Linq.Tests
 {
+    using SF = SyntaxFactory;
+
     public class CodeGenerationTests
     {
         [Test]
@@ -33,6 +38,78 @@ namespace Xml.Schema.Linq.Tests
             var cSharpNsName = Regex.Replace(namespaceNode.Name.ToString(), @"\W", "_");
 
             Assert.IsTrue(cSharpNsName == nsName);
+        }
+
+        /// <summary>
+        /// Tests the the BuildWrapperDictionary() method of the LinqToXsdTypeManager class that's
+        /// generated does not contain <c>typeof(void)</c> expressions.
+        /// </summary>
+        [Test]
+        public void NoVoidTypeOfExpressionsInXsdLinqToXsdTypeManagerBuildWrapperDictionaryMethodTest1()
+        {
+            const string atomDir = @"Schemas\Atom";
+            var atomXsdFolder = Path.Combine(Environment.CurrentDirectory, atomDir);
+            var atomRssXsdFile = $"{atomXsdFolder}\\atom.xsd";
+            var atomRssXsdFileInfo = new FileInfo(atomRssXsdFile);
+            var tree = Utilities.GenerateSyntaxTree(atomRssXsdFileInfo);
+
+            var linqToXsdTypeManagerClassDeclarationSyntax = tree.GetNamespaceRoot().DescendantNodes().OfType<ClassDeclarationSyntax>()
+                                                                 .FirstOrDefault(cds => cds.Identifier.ValueText == nameof(LinqToXsdTypeManager));
+
+            Assert.IsNotNull(linqToXsdTypeManagerClassDeclarationSyntax);
+
+            var buildWrapperDictionaryMethod = linqToXsdTypeManagerClassDeclarationSyntax
+                                               .DescendantNodes().OfType<MethodDeclarationSyntax>()
+                                               .FirstOrDefault(mds =>
+                                                   mds.Identifier.ValueText == "BuildWrapperDictionary");
+
+            Assert.IsNotNull(buildWrapperDictionaryMethod);
+
+            var statements = buildWrapperDictionaryMethod.DescendantNodes().OfType<InvocationExpressionSyntax>().ToArray();
+
+            Assert.IsTrue(statements.Length == 2);
+
+            var typeOfExpressions = statements.SelectMany(ies => ies.ArgumentList.DescendantNodes()).OfType<TypeOfExpressionSyntax>().ToArray();
+
+            Assert.IsNotEmpty(typeOfExpressions);
+            Assert.IsTrue(typeOfExpressions.Length == 4);
+
+            var typeOfVoid = SF.ParseExpression("typeof(void)");
+            var nonVoidTypeOfExpressions = typeOfExpressions.Where(toe => !toe.IsEquivalentTo(typeOfVoid)).ToArray();
+            var voidTypeOfExpressions = typeOfExpressions.Except(nonVoidTypeOfExpressions).ToArray();
+
+            Assert.IsNotEmpty(nonVoidTypeOfExpressions);
+            Assert.IsTrue(nonVoidTypeOfExpressions.Length == 2);
+
+            // if this is not empty, then you have a problem...
+            Assert.IsEmpty(voidTypeOfExpressions);
+        }
+
+        [Test]
+        public void NoVoidTypeOfExpressionsInGeneratedCodeEver()
+        {
+            var dir = new DirectoryInfo(Path.Combine(Environment.CurrentDirectory, "Schemas"));
+            var allXsds = dir.GetFiles("*.xsd", SearchOption.AllDirectories);
+
+            foreach (var xsd in allXsds) {
+                var generatedCodeTree = Utilities.GenerateSyntaxTree(xsd);
+
+                var root = generatedCodeTree.GetRoot();
+                var allDescendents = root.DescendantNodes().SelectMany(d => d.DescendantNodes());
+                var allStatements = allDescendents.OfType<StatementSyntax>();
+                var allExpressions = allStatements.SelectMany(s => s.DescendantNodes()).OfType<ExpressionSyntax>();
+                var typeOfExpressions = allExpressions.OfType<TypeOfExpressionSyntax>().ToArray();
+
+                Assert.IsNotEmpty(typeOfExpressions);
+
+                var typeOfVoid = SF.ParseExpression("typeof(void)");
+                var nonVoidTypeOfExpressions = typeOfExpressions.Where(toe => !toe.IsEquivalentTo(typeOfVoid)).ToArray();
+                var voidTypeOfExpressions = typeOfExpressions.Where(toe => toe.IsEquivalentTo(typeOfVoid)).ToArray();
+
+                Assert.IsNotEmpty(nonVoidTypeOfExpressions);
+                if (xsd.Name == "Microsoft.Build.xsd") continue; // this will have typeof(void) expressions due to other bugs
+                Assert.IsEmpty(voidTypeOfExpressions);
+            }
         }
     }
 }
