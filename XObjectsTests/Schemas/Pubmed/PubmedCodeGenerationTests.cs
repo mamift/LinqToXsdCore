45bff0d09a0c3f182630ef71f0d9581d47838438
Muhammad Miftah <mamift@gmail.com>
2020-10-31 19:41:38 +1000

    Added efetch-pubmed.xsd file for additional testing. Source: https://gist.github.com/hubgit/346707

---
+++ XObjectsTests/Schemas/Pubmed/PubmedCodeGenerationTests.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Xml.Schema.Linq.Tests.Schemas.Pubmed
+{
+    public class PubmedCodeGenerationTests
+    {
+        [Test]
+        public void TestEfetchPubmedSchemaCodeGenerates()
+        {
+            const string efetchPubmedXsd = @"Schemas\Pubmed\efetch-pubmed.xsd";
+            var efetchPubmedXsdFile = new FileInfo(efetchPubmedXsd);
+
+            Assert.DoesNotThrow(() => {
+                var syntaxTree = Utilities.GenerateSyntaxTree(efetchPubmedXsdFile);
+            });
+        }
+    }
+}
