7ecc92e046959b255783bee4a148bb6e6b7cdf4a
Muhammad Miftah <mamift@gmail.com>
2020-11-15 16:36:34 +1000

    Added IDictionaryExtensionsTests class.

---
+++ XObjectsTests/Extensions/IDictionaryExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Xml.Schema.Linq.Extensions;
+
+namespace Xml.Schema.Linq.Tests.Extensions
+{
+    [TestFixture]
+    public class IDictionaryExtensionsTests
+    {
+        [Test]
+        public void IsEqualsPositiveTest()
+        {
+            var one = new Dictionary<string, int>() {
+                {"do1", 1},
+                {"do2", 2},
+            };
+            var two = new Dictionary<string, int>() {
+                {"do1", 1},
+                {"do2", 2},
+            };
+
+            var isEquals = ((IDictionary<string, int>) one).IsEquals(((IDictionary<string, int>) two));
+
+            Assert.IsTrue(isEquals);
+        }
+
+        [Test]
+        public void IsEqualsNegativeTest()
+        {
+            var one = new Dictionary<string, int>() {
+                {"do1", 1},
+                {"do2", 1},
+            };
+            var two = new Dictionary<string, int>() {
+                {"do1", 1},
+                {"do2", 2},
+            };
+
+            var isEquals = ((IDictionary<string, int>) one).IsEquals(((IDictionary<string, int>) two));
+
+            Assert.IsFalse(isEquals);
+        }
+    }
+}
