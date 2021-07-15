496134106ece9be2b2c11aa446258595936b8da8
Muhammad Miftah <mamift@gmail.com>
2021-07-16 09:10:20 +1000

    Added a test for FileSystemUtilities.PreLoadXmlSchemas() method.

---
+++ XObjectsTests/FileSystemUtilitiesTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Schema;
+using NUnit.Framework;
+
+namespace Xml.Schema.Linq.Tests
+{
+	public class FileSystemUtilitiesTests
+	{
+		[Test]
+		public void PreLoadXmlSchemasTest()
+		{
+			var xmlSpecXsd = @"XMLSpec\xmlspec.xsd";
+			XmlSchemaSet xmlSpecSchemaSet = FileSystemUtilities.PreLoadXmlSchemas(xmlSpecXsd);
+
+			Assert.IsNotNull(xmlSpecSchemaSet);
+			Assert.IsTrue(xmlSpecSchemaSet.IsCompiled);
+			Assert.IsTrue(xmlSpecSchemaSet.Count == 3);
+		}
+	}
+}
