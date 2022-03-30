f5fee8909c17905d67cd619a66a896db9834f4fe
Muhammad Miftah <mamift@gmail.com>
2022-03-30 10:43:24 +1000

    Added unit test for using a functional constructor for a class with an inner enum type.

---
+++ XObjectsTests/XQueryXApiUseTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using W3C.XQueryX;
+
+namespace Xml.Schema.Linq.Tests;
+
+[TestFixture]
+public class XQueryXApiUseTests
+{
+    [Test]
+    public void TestFunctionalConstructorOfTypeWithInnerEnumType()
+    {
+        Assert.DoesNotThrow(() => new orderingModeDecl());
+
+        Assert.DoesNotThrow(() => {
+            var orderedName = nameof(orderingModeDeclType.ordered);
+            var unorderedName = nameof(orderingModeDeclType.unordered);
+
+            var orderingMode1 = new orderingModeDecl(orderedName);
+            var orderingMode2 = new orderingModeDecl(unorderedName);
+        });
+    }
+}
