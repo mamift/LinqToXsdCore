31882913ce477dbe76c24a96412cd7322f12c93f
Muhammad Miftah <mamift@gmail.com>
2022-03-30 11:15:38 +1000

    Fleshd out the unit tests more.

--- XObjectsTests/XQueryXApiUseTests.cs
+++ XObjectsTests/XQueryXApiUseTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using W3C.XQueryX;
 
 namespace Xml.Schema.Linq.Tests;
@@ -7,9 +8,27 @@ namespace Xml.Schema.Linq.Tests;
 public class XQueryXApiUseTests
 {
     [Test]
-    public void TestFunctionalConstructorOfTypeWithInnerEnumType()
+    public void TestFunctionalConstructorOfTypeWithInnerEnumTypeBadValue()
+    {
+        Assert.Throws<ArgumentException>(() => {
+            var orderingMode = new orderingModeDecl("badValue");
+        });
+        
+        Assert.Throws<ArgumentException>(() => {
+            var boundarySpaceDecl = new boundarySpaceDecl("badValue");
+        });
+        
+        Assert.Throws<ArgumentException>(() => {
+            var constructionDecl = new constructionDecl("badValue");
+        });
+    }
+
+    [Test]
+    public void TestFunctionalConstructorOfTypeWithInnerEnumTypeGoodValue()
     {
         Assert.DoesNotThrow(() => new orderingModeDecl());
+        Assert.DoesNotThrow(() => new boundarySpaceDecl());
+        Assert.DoesNotThrow(() => new constructionDecl());
 
         Assert.DoesNotThrow(() => {
             var orderedName = nameof(orderingModeDeclType.ordered);
@@ -17,6 +36,22 @@ public class XQueryXApiUseTests
 
             var orderingMode1 = new orderingModeDecl(orderedName);
             var orderingMode2 = new orderingModeDecl(unorderedName);
+        });
+        
+        Assert.DoesNotThrow(() => {
+            var preserveName = nameof(boundarySpaceDeclType.preserve);
+            var stripName = nameof(boundarySpaceDeclType.strip);
+
+            var boundarySpaceDecl1 = new boundarySpaceDecl(preserveName);
+            var boundarySpaceDecl2 = new boundarySpaceDecl(stripName);
+        });
+        
+        Assert.DoesNotThrow(() => {
+            var preserveName = nameof(constructionDeclType.preserve);
+            var stripName = nameof(constructionDeclType.strip);
+
+            var constructionDecl1 = new constructionDecl(preserveName);
+            var constructionDecl2 = new constructionDecl(stripName);
         });
     }
 }
