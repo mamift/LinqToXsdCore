3527601f447c0ac404f0a08f0f3f1a64ad9c8a65
Muhammad Miftah <mamift@gmail.com>
2023-09-24 16:57:40 +1000

    Removed .net 6 target. Added a new w3c XSD use test.

---
+++ XObjectsTests/XsdCodeUseTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using W3C.XSD;
+
+namespace Xml.Schema.Linq.Tests;
+
+public class XsdCodeUseTests: BaseTester
+{
+    [Test]
+    public void SchemaLangUseTest()
+    {
+        var filename = AllTestFiles.AllFiles.FirstOrDefault(f => f.Contains("XSD") && f.EndsWith("W3C XMLSchema v1.xsd"));
+        var xsdFile = AllTestFiles.FileInfo.New(filename);
+        var xsdFileXmlText = xsdFile.OpenText().ReadToEnd();
+        var d = schema.Parse(xsdFileXmlText);
+    }
+}
