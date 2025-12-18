dce06e0560ae652199f589f8d56568706d26bf20
Muhammad Miftah <mamift@gmail.com>
2025-12-18 11:58:45 +1000

    Updated biuld and regen scripts.

--- GeneratedSchemaLibraries/buildAll.ps1
+++ GeneratedSchemaLibraries/buildAll.ps1
@@ -1,28 +1,81 @@
+
 # rebuild 
 
-function start($path, $args) {
+class ProcessResult {
+    [System.Diagnostics.Process] $Process;
+    [string] $Output;
+    [string] $Errors;
+    [int] $ReturnCode;
+}
+
+function build {
+    [OutputType([ProcessResult])]
+    param(
+        [string]$path,
+        [string]$args
+    )
+    $dnb = (Get-Command dotnet)
+
     if ([System.IO.File]::Exists($path) -eq $false) {
-        throw new Exception("Erorr, path does not exist!");
+        throw new Exception("path does not exist!");
     }
 
+    #write-host $dnb.Path
+
     $pinfo = New-Object System.Diagnostics.ProcessStartInfo
-    $pinfo.FileName = $path;
+    $pinfo.FileName = $dnb.Path;
     $pinfo.RedirectStandardError = $true
     $pinfo.RedirectStandardOutput = $true
     $pinfo.UseShellExecute = $false
-    $pinfo.Arguments = "localhost"
+    $theArgs = "build $($path) -c DEBUG -v:normal";
+    if ([string]::IsNullOrWhiteSpace($args) -eq $false) {
+        $theArgs = "$($theArgs) $($args)";
+    }
+
+    #write-host $theArgs;
+    $pinfo.Arguments = $theArgs
     
     $p = New-Object System.Diagnostics.Process
     $p.StartInfo = $pinfo
-    $p.Start() | Out-Null
-    $p.WaitForExit()
-    $stdout = $p.StandardOutput.ReadToEnd()
-    $stderr = $p.StandardError.ReadToEnd()
-    Write-Host "stdout: $stdout"
-    Write-Host "stderr: $stderr"
-    Write-Host "exit code: " + $p.ExitCode
 
-    return $p;
+    [string]$stdout = [string]::Empty;
+    [string]$stderr = [string]::Empty;
+    $outputCopy = {
+        params([object]$sender, [System.Diagnostics.DataReceivedEventArgs]$eventArgs)
+
+        write-host $eventArgs.Data;
+        $stdout += $eventArgs.Data;
+    };
+    
+    Register-ObjectEvent -InputObject $p -EventName "OutputDataReceived" -Action $outputCopy;
+
+    $errorCopy = {
+        params([object]$sender, [System.Diagnostics.DataReceivedEventArgs]$eventArgs)
+
+        write-host $eventArgs.Data;
+        $stderr += $eventArgs.Data;
+    };
+    
+    Register-ObjectEvent -InputObject $p -EventName "ErrorDataReceived" -Action $errorCopy;
+
+    $p.Start();
+    $p.BeginErrorReadLine();
+    $p.BeginOutputReadLine();
+
+    $didExit = $p.WaitForExit();
+    if ($didExit -eq $false) {
+        write-error "error";
+        return;
+    }
+
+    #write-host $stdout;
+    
+    return [ProcessResult]@{
+        Process = $p
+        Output = $stdout
+        Errors = $stderr
+        ReturnCode = $p.ExitCode
+    };
 }
 
 class BuildResult {
@@ -40,24 +93,32 @@ $buildResults = New-Object System.Collections.Generic.List[BuildResult];
 $dnb = Get-Command dotnet
 
 Get-ChildItem -Attributes Directory | % {
-	if ($_ -eq "Microsoft Project 2007") { return; }
+	if ($_ -contains "Microsoft Project 2007") { return; }
 	$projects = [System.IO.Directory]::GetFiles($_.FullName, "*.csproj");
     $first = $projects[0];
 
+    write-host "Building $first...";
+
     if ($first -ne $null) {
-        $rc = start -path $dnb.Path -args "$($first) -c DEBUG -v:minimal";
+        $rc = build -path $first
 
         $result = [BuildResult]::new();
-        $result.ReturnCode = $rc.ExitCode;
+        $result.ReturnCode = $rc.ReturnCode;
         $result.Name = $first;
-        write-host $result.ToString();
         
-        $result.Output = $rc.StandardOutput.ReadToEnd();
+        if ($rc.ReturnCode -eq 1) {
+            write-host $rc.Errors;
+            return;
+        } else {
+            write-host $result.ToString();
+        }
+
+        $result.Output = $rc.Output;
+
         $buildResults.Add($result);
     } else {
         Write-Error "Unable to find CSPROJ inside folder $($_.Name)";
     }
 }
 
-
-echo $buildResults;
+echo $buildResults | select Name,ReturnCode;
---
+++ GeneratedSchemaLibraries/regenDebug.ps1
@@ -0,0 +1,3 @@
+﻿$linqtoxsd = "C:\Projects\GitHub\LinqToXsdCore\LinqToXsd\bin\Debug\net10.0\LinqToXsd.exe";
+# regenerate all CS code with this one command
+Get-ChildItem -Attributes Directory | %{ & $linqtoxsd gen $_ -a }
