4b9ad45b893b039642899470ff059e159a3e629d
Muhammad Miftah <mamift@gmail.com>
2026-01-12 14:46:53 +1000

    Merged duplicate classes.

--- XObjectsTests/Extensions/CodeDomExtensionTests.cs
+++ XObjectsTests/Extensions/CodeDomExtensionTests.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Xml;
 using System.Xml.Resolvers;
 using System.Xml.Schema;
+using Fasterflect;
 using NUnit.Framework;
+using Xml.Schema.Linq.CodeGen;
 using Xml.Schema.Linq.Extensions;
 
 namespace Xml.Schema.Linq.Tests.Extensions
@@ -13,6 +15,27 @@ namespace Xml.Schema.Linq.Tests.Extensions
     [TestFixture]
     public class CodeDomExtensionTests: BaseTester
     {
+        [Test]
+        public void TestIsEquivalentTypeReference()
+        {
+            var xmlSchemaElement = new XmlSchemaType() {
+                Name = "test"
+            };
+
+            var exampleClrTypeRef = new ClrTypeReference(nameof(String), typeof(string).Namespace,
+                xmlSchemaElement, false, false);
+
+            exampleClrTypeRef.SetFieldValue("clrName", "String");
+            exampleClrTypeRef.SetFieldValue("typeNs", "System");
+            exampleClrTypeRef.SetFieldValue("clrFullTypeName", typeof(string).FullName);
+
+            var codeTypeRef = new CodeTypeReference(typeof(string));
+
+            var isEquivalent = exampleClrTypeRef.IsEquivalentTypeReference(codeTypeRef);
+
+            Assert.True(isEquivalent);
+        }
+
         [Test]
         public void IsEquivalentEnumDeclarationTestTrue()
         {
