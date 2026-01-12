047adff12e4aa51ca066534b7a1f2fcff4230e83
Muhammad Miftah <mamift@gmail.com>
2026-01-12 13:25:23 +1000

    Test cleanup.

--- XObjectsTests/CodeGen/ClrPropertyTests.cs
+++ XObjectsTests/CodeGen/ClrPropertyTests.cs
@@ -11,28 +11,6 @@ namespace Xml.Schema.Linq.Tests.CodeGen;
 public class ClrPropertyTests: BaseTester
 {
     [Test]
-    public void T1()
-    {
-        MockFileInfo abstractTestXsd = AllTestFiles.GetMockFileInfo(f => f.EndsWith("abstracttest.xsd"));
-        MockFileInfo abstractTestXsdConfig = AllTestFiles.GetMockFileInfo(f => f.EndsWith("abstracttest.xsd.config"));
-        var config = Configuration.Load(abstractTestXsdConfig.ToStreamReader());
-
-        var schemaSet = Utilities.GetXmlSchemaSet(abstractTestXsd, AllTestFiles);
-        var defaultSettings = schemaSet.ToDefaultMergedConfiguration(config).ToLinqToXsdSettings();
-        var xsdConverter = new XsdToTypesConverter(defaultSettings);
-        ClrMappingInfo? mapping = xsdConverter.GenerateMapping(schemaSet);
-
-        var codeGenerator = new CodeDomTypesGenerator(defaultSettings);
-        var namespaces = codeGenerator.GenerateTypes(mapping).ToList();
-
-        Assert.NotNull(mapping);
-
-        foreach (var type in mapping.Types) {
-            
-        }
-    }
-
-    [Test]
     public void TestSimpleTypeUnionMappingNames()
     {
         MockFileInfo akk = AllTestFiles.GetMockFileInfo(f => f.EndsWith("AkomaNtoso\\akomantoso30.xsd"));
