bba2e7f6d8ad8cb58f5af25f4c0b6477f69076ff
Muhammad Miftah <mamift@gmail.com>
2026-01-12 13:25:27 +1000

    wip.

---
+++ XObjectsTests/CodeGen/ClrTypeInfoTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Xml.Schema.Linq.CodeGen;
+using XObjects;
+
+namespace Xml.Schema.Linq.Tests.CodeGen;
+
+public class ClrTypeInfoTests: BaseTester
+{
+    [Test, TestCase("StuDateAndTime.xsd")]
+    public void CreateSimpleTypeForAnonymousSimpleTypeUnion(string endsWithFilePattern)
+    {
+        var xsd = GetTestFileAsXmlSchemaSet(endsWithFilePattern);
+
+        var anonUnionTypes = xsd.RetrieveAllAnonymousSimpleUnionTypes();
+
+        Assert.NotNull(anonUnionTypes);
+
+        foreach (var simpleType in anonUnionTypes) {
+            var type = ClrSimpleTypeInfo.CreateSimpleTypeInfo(simpleType.Value);
+        }
+    }
+}
