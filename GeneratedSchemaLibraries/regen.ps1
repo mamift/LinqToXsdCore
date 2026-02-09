fd9fa1761e5468dc332b848f78acac0180dd6ca8
Muhammad Miftah <mamift@gmail.com>
2026-02-10 09:41:24 +1000

    Updated regen.ps1 to use current source code via dotnet command. added xqueryx debug target.

--- GeneratedSchemaLibraries/regen.ps1
+++ GeneratedSchemaLibraries/regen.ps1
@@ -1,2 +1,10 @@
+
+$cwd = [Environment]::CurrentDirectory;
+
+$linqToXsdProject = [System.IO.Path]::Combine($cwd, "..\LinqToXsd\LinqToXsd.csproj")
+
 # regenerate all CS code with this one command
-Get-ChildItem -Attributes Directory | %{ LinqToXsd gen $_ -a }
+Get-ChildItem -Attributes Directory | % {    
+    dotnet run --no-launch-profile --framework net8.0 --project $linqToXsdProject -- gen $_ -a
+    #LinqToXsd gen $_ -a 
+}
