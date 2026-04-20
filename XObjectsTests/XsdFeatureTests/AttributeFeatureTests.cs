e8d0f15d90ca91fd959054b90f237fb7b55cfc9b
Muhammad Miftah <mamift@gmail.com>
2026-04-20 21:08:56 +1000

    added test for ListTypeWithDefaults.xsd

---
+++ XObjectsTests/XsdFeatureTests/AttributeFeatureTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace Xml.Schema.Linq.Tests.XsdFeatureTests;
+
+public class AttributeFeatureTests
+{
+    [Test]
+    public void AttributeFeatureTestOtherNames1()
+    {
+        var xml = """
+                  <person xmlns="urn:LinqToXsdCore:ListTypeWithDefaults" otherNames="mono duo trio">
+                  </person>
+                  """;
+
+        var person = urn.LinqToXsdCore.ListTypeWithDefaults.person.Parse(xml);
+
+        Assert.NotNull(person.otherNames);
+
+        Assert.True(person.otherNames.Contains("mono"));
+        Assert.True(person.otherNames.Contains("duo"));
+        Assert.True(person.otherNames.Contains("trio"));
+    }
+
+    [Test]
+    public void AttributeFeatureTestOtherNames2()
+    {
+        var xml = """
+                  <person xmlns="urn:LinqToXsdCore:ListTypeWithDefaults" otherNames="">
+                  </person>
+                  """;
+
+        var person = urn.LinqToXsdCore.ListTypeWithDefaults.person.Parse(xml);
+
+        Assert.NotNull(person.otherNames);
+        Assert.True(person.otherNames.Count == 0);
+    }
+
+    [Test]
+    public void AttributeFeatureTestTagsDefaults()
+    {
+        var xml = """
+                  <person xmlns="urn:LinqToXsdCore:ListTypeWithDefaults">
+                  </person>
+                  """;
+
+        var person = urn.LinqToXsdCore.ListTypeWithDefaults.person.Parse(xml);
+
+        Assert.NotNull(person.tags);
+        Assert.True(person.tags.Count == 2);
+    }
+}
