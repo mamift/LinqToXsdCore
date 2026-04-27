914b3dc4a1233db3e7047d3149fae4dfa5145656
Muhammad Miftah <mamift@gmail.com>
2026-04-27 14:39:07 +1000

    Yet another bug found and fixed by claude - fixed a bug in code gen, when referencing an enum for the internal static field holding a default value, the member reference was syntactically uncompilable.

--- XObjectsCode/Src/SimpleTypeCodeDomHelper.cs
+++ XObjectsCode/Src/SimpleTypeCodeDomHelper.cs
@@ -420,7 +420,8 @@ namespace Xml.Schema.Linq.CodeGen
             }
             else if (isEnum)
             {
-                return new CodeFieldReferenceExpression(new CodeTypeReferenceExpression(builtInType), strValue);
+                var enumFacet = new EnumFacet(strValue);
+                return new CodeFieldReferenceExpression(new CodeTypeReferenceExpression(builtInType), enumFacet.Member);
             }
             else if (localType == "Uri")
             {
