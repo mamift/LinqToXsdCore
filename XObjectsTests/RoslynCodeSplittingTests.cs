5b040ed96db4c7c5f5b7ef83d0899b1abbe9378d
Muhammad Miftah <mamift@gmail.com>
2026-05-05 13:52:21 +1000

    some refinements to the algorithm.

--- XObjectsTests/RoslynCodeSplittingTests.cs
+++ XObjectsTests/RoslynCodeSplittingTests.cs
@@ -30,19 +30,24 @@ internal class RoslynCodeSplittingTests
         CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
 
         // Gather shared usings/namespace context
-        var usings = root.Usings.ToFullString();
+        var usings = root.Usings.Select(uds => uds.ToFullString().Trim()).ToList();
 
         foreach (var member in root.Members) {
             if (member is BaseNamespaceDeclarationSyntax ns) {
+                if (ns is NamespaceDeclarationSyntax nsds) {
+                    var nsUsings = nsds.Usings.Select(uds => uds.ToFullString().Trim());
+                    usings.AddRange(nsUsings);
+                }
+
                 foreach (var typeMember in ns.Members.OfType<BaseTypeDeclarationSyntax>()) {
                     string outputFile = Path.Combine(projectFolder.FullName, $"{typeMember.Identifier.Text}.cs");
-                    var content = $"{usings}\nnamespace {ns.Name};\n\n{typeMember.ToFullString()}";
+                    var content = $"{string.Join("\n", usings)}\nnamespace {ns.Name}\n{{ \n\n{typeMember.ToFullString()}\n}}";
                     File.WriteAllText(outputFile, content);
                 }
             }
             else if (member is BaseTypeDeclarationSyntax type) {
                 string outputFile = Path.Combine(projectFolder.FullName, $"{type.Identifier.Text}.cs");
-                var content = $"{usings}\n{type.ToFullString()}";
+                var content = $"{string.Join("\n", usings)}\n{type.ToFullString()}";
                 File.WriteAllText(outputFile, content);
             }
         }
