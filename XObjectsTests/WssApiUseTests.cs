d78d484ffb3289440fb3a27a78b9c01850b1099d
Muhammad Miftah <mamift@gmail.com>
2026-05-08 15:49:30 +1000

    added another test to cover fix in previous commit.

--- XObjectsTests/WssApiUseTests.cs
+++ XObjectsTests/WssApiUseTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 using Microsoft.Schemas.SharePoint;
@@ -66,6 +67,42 @@ namespace Xml.Schema.Linq.Tests
             XAttribute underlyingValue = newParamLocal.Untyped.Attributes(designerTypeAttr).First();
             const string expectedValue = nameof(parametersType.ParameterLocalType.DesignerTypeEnum.DataSourceFieldNames);
             Assert.IsTrue(underlyingValue.Value == expectedValue);
+        }
+
+        /// <summary>
+        /// The purpose of this test is to ensure when getting the value of <see cref="Feature.Hidden"/> (which is an enum type), no exceptions occur when parsing and returning
+        /// the enum string value (which is what happens under the hood with the XObjects API as it invokes <see cref="Enum.Parse(System.Type,System.string)"/>).
+        /// <para>lower case 'false' does clash with C# keyword.</para>
+        /// </summary>
+        [Test]
+        public void TrueFalseTest_EnumValueClashesWithCSharpKeyword()
+        {
+            var newF = new Feature(new FeatureDefinition() {
+                Hidden = TRUEFALSE.@false
+            });
+
+            var isHidden = newF.Hidden;
+
+            Assert.NotNull(isHidden);
+            Assert.AreEqual(isHidden, TRUEFALSE.@false);
+        }
+
+        /// <summary>
+        /// The purpose of this test is to ensure when getting the value of <see cref="Feature.Hidden"/> (which is an enum type), no exceptions occur when parsing and returning
+        /// the enum string value (which is what happens under the hood with the XObjects API as it invokes <see cref="Enum.Parse(System.Type,System.string)"/>).
+        /// <para>Upper case 'TRUE' does not clash with C# keyword.</para>
+        /// </summary>
+        [Test]
+        public void TrueFalseTest_EnumValueIsNotCSharpKeyword()
+        {
+            var newF = new Feature(new FeatureDefinition() {
+                Hidden = TRUEFALSE.TRUE
+            });
+
+            var isHidden = newF.Hidden;
+
+            Assert.NotNull(isHidden);
+            Assert.AreEqual(isHidden, TRUEFALSE.TRUE);
         }
     }
 }
